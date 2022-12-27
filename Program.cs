1eb39f22df17f328497d6417adf11339a01dc1c5
Nastya <anastasiya.komova@mail.ru>
2022-12-27 11:57:03 +0300

    Задача 58

--- Program.cs
+++ Program.cs
@@ -108,6 +108,71 @@ minRows(myArray);
 */
 
 //Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int [,] CreateArray(int rows, int columns)
+{   
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array[i,j] = new Random().Next(1,11);
+    return array;
+}
+
+void ShowMatrix(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j]+"  ");
+        Console.WriteLine(" ");
+    }
+    Console.WriteLine();
+}
+
+int [,] multiMatrix (int[,] array, int[,] array2)
+{
+int [,] arrayC = new int[array.GetLength(0), array2.GetLength(1)];
+for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array2.GetLength(0); k++)
+            {
+                {
+                    arrayC[i,j] += array [i,k] * array2 [k,j];
+                }
+            }
+        }
+    }
+return arrayC;
+}
+
+Console.Write("Input a number of rows matrix №1: ");
+int rowsMatrix1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a number of columns matrix №1: ");
+int columnsMatrix1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a number of rows matrix №2: ");
+int rowsMatrix2= Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a number of columns matrix №2: ");
+int columnsMatrix2 = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateArray(rowsMatrix1,columnsMatrix1);
+ShowMatrix(myArray);
+int[,] myArray2 = CreateArray(rowsMatrix2,columnsMatrix2);
+ShowMatrix(myArray2);
+
+if (columnsMatrix1 != rowsMatrix2)
+Console.Write("Error! Columns matrix №1 ≠ rows matrix №2");
+else 
+{
+    Console.WriteLine("Final matrix:");
+    int[,] myArray3 = multiMatrix(myArray,myArray2);
+    ShowMatrix(myArray3);
+}
+
 //Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 //Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4. 
 //любой массив.
